1b991ffd878278071e45c8a1c67465c80d33beaa
Pelayo Méndez <pelayo.mendez@enlighting-tech.com>
2020-11-13 17:28:37 +0100

    First commit

---
+++ precache-manifest.5c7d5b7cba858a93862e6389aefe629d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045f80697c81d04af9e925d27a2ad19a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "739335e2d0d7fd945e89",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3a8279d70555a0423cb",
+    "url": "/static/js/2.d47ec4b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d47ec4b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739335e2d0d7fd945e89",
+    "url": "/static/js/main.a769bea0.chunk.js"
+  },
+  {
+    "revision": "c8cca8008ea197fe5312",
+    "url": "/static/js/runtime-main.3ab5f277.js"
+  }
+]);
